96c99198a2b20a3f320c9ad1f29400c08fd08c7a
Abd AL Rahman Miassar <abodimiassar@gmail.com>
2026-04-07 14:59:21 +0300

    Implement the slugification algorithm.

--- ClassLibrary-Slug Generator-Abd/SlugGenerator.cs
+++ ClassLibrary-Slug Generator-Abd/SlugGenerator.cs
@@ -4,7 +4,39 @@
     {
         public static string Generate(string text)
         {
-            //I forgot to make brunch for Pr 
+            text = text.ToLower();
+
+            int start = 0;
+            while (start < text.Length && !Char.IsLetter(text[start]))
+                start++;
+
+            int end = text.Length - 1;
+            while (end >= 0 && !Char.IsLetter(text[end]))
+                end--;
+
+            text = text.Substring(start, end - start + 1);
+
+            string slug = "";
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (Char.IsLetter(text[i]))
+                    slug += text[i];
+                else
+                {
+                    int j = i;
+                    bool isSeparator = false;
+                    while (j < text.Length && !Char.IsLetter(text[j]))
+                    {
+                        if (text[j] == ' ' || text[j] == '_')
+                            isSeparator = true;
+                        j++;
+                    }
+                    if (isSeparator)
+                        slug += '-';
+                    i = j - 1;
+                }
+            }
+            return slug;
         }
     }
 }
